49e642bba1f67061b999abcfde28b218dbc58f5b
diwaker321 <diwakeracadmic@gmail.com>
2026-08-22 08:36:09 +0530

    fetch code has been pushed

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 // callback
 // test case
 
+// import { useEffect, useState } from "react";
+
 // login --> order ---> payment ---> invoice
 // diwaker
 //order id
@@ -24,17 +26,24 @@
 
 //promise ---> object---> represent eventual complition / failure -->function
 
-// const getpayment = new Promise((resolve, reject) => {
+// const getpayment = new Promise((resolve, reject , orderid) => {
+//     console.log(orderid);
+
 //     //logic
-// //   let success = true;
-// //   if (success) {
-// //     resolve("Payment done");
-// //   } else {
-// //     reject("Got an error, please try again later");
-// //   }
-//     setTimeout(()=>{
-//         resolve("payment has been done")
-//     },3000)
+//   let success = true;
+//   if (success) {
+//     const paymentdetials = {
+//         name:"diwaker",
+//         items:["shoes" , "bags" , " phone"],
+//         totalPrice :12000
+//     }
+//     resolve(paymentdetials)
+//   } else {
+//     reject("Got an error in payment method, please try again later");
+//   }
+//     // setTimeout(()=>{
+//     //     resolve("payment has been done")
+//     // },3000)
 // });
 
 //how to consume your promise
@@ -63,5 +72,89 @@
 //     } )
 // })
 
-// promise chaining and callback hell diff. 
+// getUser().then((user) => {
+//     console.log(user);
+
+//    return getorder(user)
+// }).then((orderid)=>{
+//     console.log(orderid);
+
+//     return getpayment(orderid)
+// }).then((paymentdetials)=>{
+//     console.log(paymentdetials);
+
+//     return getinvoice(paymentdetials)
+// }).then(()=>{
+//     console.log("finished");
+// }).catch((error)=>{
+//     console.log(error);
+// })
+
+// function getUser() {
+// //   setTimeout(()=>{
+// //       return Promise.resolve("Diwaker")
+// //   },2000)
+// //   return Promise.resolve("Diwaker");
+// }
+
+// function getorder() {
+//   return Promise.resolve("bags");
+// }
+
+// function getpayment() {
+//   return Promise.resolve("2000");
+// }
+
+// async function getData() {
+//   try {
+//     const username = await getUser();
+//     const orderitems = await getorder();
+//     const paymentamount = await getpayment();
+
+//     console.log(username);
+//     console.log(orderitems);
+//     console.log(paymentamount);
+//   } catch (err) {
+//     console.log(err);
+//   }
+// }
+
+// getData();
+
+// getUser()
+
+// getUser().then((user)=>{
+//     console.log(user);
+//     return getorder()
+// }).then((selectedOrder)=>{
+//     console.log(selectedOrder);
+//     return getpayment()
+// }).then((payemntAmount)=>{
+//     console.log(payemntAmount);
+// })
+
+// h.w
+// Promise
+
+//async and await keyword
+
+// fetch  -->promise
+// .then .catch
+// api call
+// data r
+
+// promise chaining and callback hell diff.
+//async and await
+//api calling
 //hooks
+// useState and useEffect
+
+async function getdata() {
+  const data = await fetch(
+    "https://www.themealdb.com/api/json/v1/1/search.php?f=k",
+  );
+  const res = await data.json();
+  console.log(res);
+}
+
+getdata();
